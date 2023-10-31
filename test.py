f315e78e60d6999425ada79c90f3c393b83726d2
James Richardson <james@time4tea.net>
2023-10-31 20:59:36 +0000

    cache some bits

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
+import base64
 import dataclasses
+import gzip
+import json
+from collections import defaultdict
+from functools import cached_property
 
 import requests
 from geotiler import Map
 from geotiler.geo import WebMercator
 from geotiler.map import _find_top_left_tile, _tile_coords, _tile_offsets
 from geotiler.provider import MapProvider
+from mapbox_vector_tile.decoder import TileData
 from pmtiles.reader import Reader
+from pmtiles.tile import deserialize_header, Compression, zxy_to_tileid, deserialize_directory, find_tile
 from sqlitedict import SqliteDict
 
 
@@ -72,17 +79,81 @@ class RequestsSource:
         return self.cache[key]
 
 
-class PMReader(Reader):
+class keydefaultdict(defaultdict):
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        else:
+            ret = self[key] = self.default_factory(key)
+            return ret
+
+
+class PMReader:
 
     def __init__(self, get_bytes):
-        super().__init__(get_bytes)
+        self.get_bytes = get_bytes
+        self.directories = keydefaultdict(lambda ol: deserialize_directory(self.get_bytes(ol[0], ol[1])))
 
     def xyz(self, xyz: XYZ):
-        return super().get(xyz.z, xyz.x, xyz.y)
+        return self.get(xyz.z, xyz.x, xyz.y)
+
+    @cached_property
+    def header(self):
+        return deserialize_header(self.get_bytes(0, 127))
+
+    @cached_property
+    def metadata(self):
+        header = self.header
+        metadata = self.get_bytes(header["metadata_offset"], header["metadata_length"])
+        if header["internal_compression"] == Compression.GZIP:
+            metadata = gzip.decompress(metadata)
+        return json.loads(metadata)
+
+    @cached_property
+    def _root_offset(self):
+        return self.header["root_offset"]
+
+    @cached_property
+    def _root_length(self):
+        return self.header["root_length"]
+
+    @cached_property
+    def _leaf_directory_offset(self):
+        return self.header["leaf_directory_offset"]
+
+    @cached_property
+    def _tile_data_offset(self):
+        return self.header["tile_data_offset"]
+
+    @cached_property
+    def _tile_compression(self) -> Compression:
+        return self.header["tile_compression"]
+
+    def _decompress_tile(self, d: bytes) -> bytes:
+        if self._tile_compression == Compression.NONE:
+            return d
+        elif self._tile_compression == Compression.GZIP:
+            return gzip.decompress(d)
+
+    def get(self, z, x, y):
+        tile_id = zxy_to_tileid(z, x, y)
+        dir_offset = self._root_offset
+        dir_length = self._root_length
+        for depth in range(0, 4):  # max depth
+            directory = self.directories[(dir_offset, dir_length)]
+            result = find_tile(directory, tile_id)
+            if result:
+                if result.run_length == 0:
+                    dir_offset = self._leaf_directory_offset + result.offset
+                    dir_length = result.length
+                else:
+                    return self._decompress_tile(
+                        self.get_bytes(self._tile_data_offset + result.offset, result.length)
+                    )
 
 
 if __name__ == "__main__":
-    map = PMMap(center=(51.445114, -0.264333), zoom=12, size=(500, 500))
+    map = PMMap(center=(-0.264333, 51.445114), zoom=12, size=(500, 500))
 
     print(map.tiles())
 
@@ -95,4 +166,6 @@ if __name__ == "__main__":
         reader = PMReader(source.get_bytes)
 
         for tile in map.tiles():
-            reader.xyz(tile.locator)
+            b = reader.xyz(tile.locator)
+            d = TileData(b)
+            print(d)
